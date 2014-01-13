b386923db0f8500edab1442c62f71d6dc40e9398
Edgar Meinart <edgars@meinarts.name>
2014-01-13 23:23:30 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapistranoTest::Application.config.secret_key_base = '84bf54b23482543400979cbfb4a2a810e1b257de97eaa3d4d427414dfb54c0b82ea6b0602bd4acd0640dc47cd721415bfb0f87b5b28a7ba70548cbe111293dbb'
